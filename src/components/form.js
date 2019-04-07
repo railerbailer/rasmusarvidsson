dd1354c5eb13d6dfbf40268c2a73a771f26512f9
railerbailer <rasmus.arvidsson1@gmail.com>
2019-04-07 21:44:12 +0200

    rmoved unneccaasarary js

--- src/components/form.js
+++ src/components/form.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import { Form, Input, TextArea, Button } from 'semantic-ui-react'
-<Form.Field type='email' id='form-input-control-last-name' control={Input} label='Email' placeholder='Example@example.com' />
+import React from "react";
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+<Form.Field
+  type="email"
+  id="form-input-control-last-name"
+  control={Input}
+  label="Email"
+  placeholder="Example@example.com"
+/>;
 const ContactForm = () => (
   <Form>
-    <Form.Group widths='equal'>
-      <Form.Field type='text' id='form-input-control-first-name' control={Input} label='Name' placeholder='Name' />
-    
-      <Form.Field type='email' id='form-input-control-last-name' control={Input} label='Email' placeholder='Example@example.com' />
-    
+    <Form.Group widths="equal">
+      <Form.Field
+        type="text"
+        id="form-input-control-first-name"
+        control={Input}
+        label="Name"
+        placeholder="Name"
+      />
+
+      <Form.Field
+        type="email"
+        id="form-input-control-last-name"
+        control={Input}
+        label="Email"
+        placeholder="Example@example.com"
+      />
     </Form.Group>
-    <Form.Field id='form-textarea-control-opinion' control={TextArea} label='Message' placeholder='Message' />
-    <Form.Field id='form-button-control-public' control={Button} content='Confirm' label='Label with htmlFor' />
+    <Form.Field
+      id="form-textarea-control-opinion"
+      control={TextArea}
+      label="Message"
+      placeholder="Message"
+    />
+    <Form.Field
+      id="form-button-control-public"
+      control={Button}
+      content="Confirm"
+      label="Label with htmlFor"
+    />
   </Form>
-)
+);
 
 export default ContactForm;
